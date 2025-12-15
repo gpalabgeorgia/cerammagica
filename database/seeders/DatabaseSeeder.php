249329b4331f1c229d2b7c2ad2627ec7096707d0
gpalabgeorgia <142142478+gpalabgeorgia@users.noreply.github.com>
2025-12-15 17:45:52 +0100

    Categories db and table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         // $this->call(AdminsTableSeeder::class);
-            $this->call(SectionsTableSeeder::class);
+//            $this->call(SectionsTableSeeder::class);
+            $this->call(CategoryTableSeeder::class);
     }
 }
