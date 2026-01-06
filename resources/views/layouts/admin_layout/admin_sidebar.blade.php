6240d051e762d028ad07913ba6b7b60a86ef0f18
gpalabge <142142478+gpalabgeorgia@users.noreply.github.com>
2026-01-06 21:40:30 +0100

    Clean admin header and admin sidebar pages code

--- resources/views/layouts/admin_layout/admin_sidebar.blade.php
+++ resources/views/layouts/admin_layout/admin_sidebar.blade.php
@@ -1,8 +1,4 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="#" class="brand-link">
-        <img src="{{ asset('images/admin_images/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Admin</span>
-    </a>
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
@@ -10,16 +6,6 @@
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ Auth::guard('admin')->user()->name }}</a>
-            </div>
-        </div>
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
             </div>
         </div>
         <nav class="mt-2">
