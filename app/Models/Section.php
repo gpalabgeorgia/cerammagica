3786811dd95fe0af8473c7f60f72e406fd3e9911
gpalabge <142142478+gpalabgeorgia@users.noreply.github.com>
2026-01-14 15:33:27 +0100

    Make a dynamic header menu

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -10,6 +10,12 @@ class Section extends Model
     use HasFactory;
     protected $guard = 'sections';
 
+    public static function sections() {
+        $getSections = Section::with('categories')->where('status', 1)->get();
+        $getSections = json_decode(json_encode($getSections));
+        return $getSections;
+    }
+
     public function categories() {
         return $this->hasMany('App\Models\Category', 'section_id')->where(['parent_id'=>'Родительская', 'status'=>1])->with('subcategories');
     }
--- resources/views/layouts/front_layout/front_header.blade.php
+++ resources/views/layouts/front_layout/front_header.blade.php
@@ -1,3 +1,8 @@
+<?php
+    use App\Models\Section;
+    $sections = Section::sections();
+//    echo "<pre>"; print_r($sections); die;
+?>
 <div class="header-top-1">
     <div class="container">
         <div class="header-top-wrapper">
@@ -25,85 +30,6 @@
         </div>
     </div>
 </div>
-
-<!-- Sticky Header Section start  -->
-<header class="header-1 sticky-header">
-    <div class="mega-menu-wrapper">
-        <div class="header-main">
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 col-md-6 col-lg-10 col-xl-8 col-xxl-10">
-                        <div class="header-left">
-                            <div class="logo">
-                                <a href="#" class="header-logo">
-                                    <img src="{{ asset('images/front_images/logo/white-logo.svg') }}" alt="logo-img">
-                                </a>
-                            </div>
-                            <div class="mean__menu-wrapper">
-                                <div class="main-menu">
-                                    <nav>
-                                        <ul>
-                                            <li>
-                                                <a href="#">Inicio</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Tienda</a>
-                                                <ul class="submenu">
-                                                    <li><a href="#">Tienda</a></li>
-                                                    <li><a href="#">Listas</a></li>
-                                                    <li><a href="#">Detalles</a></li>
-                                                    <li><a href="#">Carrito</a></li>
-                                                    <li><a href="#">Deseos</a></li>
-                                                    <li><a href="#">Pagar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="has-dropdown">
-                                                <a href="#">Categorías <i class="fas fa-angle-down"></i></a>
-                                                <ul class="submenu">
-                                                    <li><a href="#">Sobre mi</a></li>
-                                                    <li class="has-dropdown">
-                                                        <a href="#">
-                                                            Author
-                                                            <i class="fas fa-angle-down"></i>
-                                                        </a>
-                                                        <ul class="submenu">
-                                                            <li><a href="#">Autor</a></li>
-                                                            <li><a href="#">Author Profile</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li><a href="#">Pregutas</a></li>
-                                                    <li><a href="#">404 Page</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Blog</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-6 col-lg-2 col-xl-4 col-xxl-2">
-                        <div class="header-right">
-                            <div class="menu-cart">
-                                <a href="#" class="cart-icon">
-                                    <i class="fa-solid fa-cart-plus"></i>
-                                </a>
-                                <div class="header-humbager ml-30">
-                                    <a class="sidebar__toggle" href="javascript:void(0)">
-                                        <div class="bar-icon-2">
-                                            <img src="{{ asset('images/front_images/icon/icon-13.svg') }}" alt="img">
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
-
 <!-- Main Header Section start  -->
 <header class="header-1">
     <div class="mega-menu-wrapper">
@@ -121,24 +47,23 @@
                                 <div class="main-menu">
                                     <nav id="mobile-menu">
                                         <ul>
-                                            <li>
-                                                <a href="#">
-                                                    Inicio
-                                                    <i class="fas fa-angle-down"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Tienda<i class="fas fa-angle-down"></i></a>
-                                            </li>
-                                            <li class="has-dropdown">
-                                                <a href="about.html">Categorías<i class="fas fa-angle-down"></i></a>
+                                            <li><a href="#">Inicio</a></li>
+                                            @foreach($sections as $section)
+                                                @if(count($section->categories)>0)
+                                                    <li><a href="#">{{ $section->name }}</a><i class="fas fa-angle-down"></i>
                                                 <ul class="submenu">
-                                                    <li><a href="about.html">Sobre mí</a></li>
-                                                    <li class="has-dropdown"><a href="team.html">Autor</a></li>
-                                                    <li><a href="faq.html">FAQ</a></li>
-                                                    <li><a href="404.html">404 Page</a></li>
+                                                    @foreach($section->categories as $category)
+                                                    <li><a href="#">{{ $category->category_name }}</a></li>
+                                                    @endforeach
+{{--                                                    <li><a href="#">Listas</a></li>--}}
+{{--                                                    <li><a href="#">Detalles</a></li>--}}
+{{--                                                    <li><a href="#">Carrito</a></li>--}}
+{{--                                                    <li><a href="#">Deseos</a></li>--}}
+{{--                                                    <li><a href="#">Pagar</a></li>--}}
                                                 </ul>
                                             </li>
+                                                @endif
+                                            @endforeach
                                             <li><a href="news.html">Blog</a></li>
                                         </ul>
                                     </nav>
